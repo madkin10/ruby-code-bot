5226b0e2b825255bc83533bf561d0e59a31ca085
Marco Adkins <madkins@virtualhold.com>
2019-01-09 09:42:04 -0500

    Remove share button

--- ruby_code_bot.rb
+++ ruby_code_bot.rb
@@ -12,21 +12,7 @@ class RubyCodeBot < Sinatra::Base
 
   post '/execute' do
     content_type :json
-    share_url = "https://ruby-code-bot.herokuapp.com/share?token=#{params[:token]}&text=#{CGI.escape(params[:text])}"
     response = { attachments: [{ title: 'Code:', text: params['text'] }] }
-    result = SafeRuby.eval(params['text'])
-    response[:attachments] << { color: 'good', title: 'Result:', text: result.to_s }
-    response[:attachments] << { actions: [type: 'button', text: 'Share', url: share_url] }
-    response.to_json
-  rescue SyntaxError, StandardError => e
-    response[:attachments] << { color: 'danger', title: 'Exception:', text: e.message }
-    response[:attachments] << { actions: [type: 'button', text: 'Share', url: share_url] }
-    response.to_json
-  end
-
-  get '/share' do
-    content_type :json
-    response = { response_type: 'in_channel', attachments: [{ title: 'Code:', text: params['text'] }] }
     result = SafeRuby.eval(params['text'])
     response[:attachments] << { color: 'good', title: 'Result:', text: result.to_s }
     response.to_json
