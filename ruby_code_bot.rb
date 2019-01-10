f84748b7f41e00a417ff6818ec7eb532a680e6ac
Marco Adkins <madkins@virtualhold.com>
2019-01-10 09:04:31 -0500

    Forgot to get token from params

--- ruby_code_bot.rb
+++ ruby_code_bot.rb
@@ -8,7 +8,7 @@ class RubyCodeBot < Sinatra::Base
   SLACK_TOKENS = ENV['SLACK_TOKENS']&.split || []
 
   before do
-    halt 401 unless SLACK_TOKENS.include?(token)
+    halt 401 unless SLACK_TOKENS.include?(params[:token])
   end
 
   post '/execute' do
